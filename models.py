e5f5b9236056dfe2520f4bb55e02a0042c83fcef
matveym <matvey.maslov@gmail.com>
2013-09-14 21:12:30 +0300

    GAE: config & models

---
+++ models.py
@@ -0,0 +1,18 @@
+from google.appengine.ext import ndb
+
+
+def init_localstore():
+    from google.appengine.ext import testbed
+    testbed = testbed.Testbed()
+    testbed.activate()
+    testbed.init_memcache_stub()
+    testbed.init_datastore_v3_stub()
+
+
+class Account(ndb.Model):
+    name = ndb.StringProperty()
+    url = ndb.StringProperty()
+    login = ndb.StringProperty()
+    # TODO Make BlobProperty, encrypt
+    password = ndb.StringProperty()
+    notes = ndb.StringProperty()

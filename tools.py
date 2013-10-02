630aaaec7e4c8b1ce1e8e126c91041bf90979c94
matveym <matvey.maslov@gmail.com>
2013-10-02 12:35:01 +0300

    tools.py: refactor import

--- tools.py
+++ tools.py
@@ -1,19 +1,23 @@
 from google.appengine.ext import ndb
 
-from models import Site, root_key
+from models import Site, root_key, all_sites
 
 
-def import_file():
+def import_file(name='data.txt'):
     lines = []
-    with open('data.txt') as f:
+    with open(name) as f:
         lines = f.readlines()
 
     import_data(lines)
 
 
-def import_data(text):
-    delete_all()
+def read_file(name):
+    with open(name) as f:
+        return ''.join(f.readlines()).decode('utf8')
 
+
+def parse_data(text):
+    result = []
     for line in text.splitlines():
         line = line.strip()
         try:
@@ -26,10 +30,20 @@ def import_data(text):
                 site_name, password = line.split(',')
                 login = ''
                 notes = ''
-        Site(name=site_name, url='', login=login, password=password, notes=notes,
-                parent=root_key).put()
+            site_name, login, password, notes = strip_all(site_name, login, password,
+                    notes)
+            result.append(Site(name=site_name, url='', login=login, password=password, notes=notes,
+                parent=root_key))
+    return result
+
+
+def import_data(text):
+    delete_all()
+
+    for site in parse_data(text):
+        site.put()
 
 
 def delete_all():
-    keys = [site.key for site in Site.query()]
+    keys = [site.key for site in all_sites()]
     ndb.delete_multi(keys)
